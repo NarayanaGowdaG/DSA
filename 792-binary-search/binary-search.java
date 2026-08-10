68ec708a1273bf8e152cbfb6b666f19fc4547ea5
NarayanaGowdaG <narayanagowda658@gmail.com>
2026-08-10 21:20:39 +0530

    Time: 0 ms (100.00%) | Memory: 48.2 MB (70.07%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -1,14 +1,13 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int low=0;
-        int high=nums.length-1;
-        while(low<=high){
-           int mid=(low+high)/2;
-            if(nums[mid]==target)return mid;
-            else if(target>nums[mid])low=mid+1;
-            else high=mid-1;
-        }
-     return -1;  
+        return func(nums,0,nums.length-1,target);
+       }
+       private int func(int[] nums,int low,int high,int target){
+        if(low>high)return -1;
+        int mid=(low+high)/2;
+        if(nums[mid]==target)return mid;
+        else if(target>nums[mid])return func(nums,mid+1,high,target);
+        return func(nums,low,mid-1,target);
+       }
         
     }
-}
