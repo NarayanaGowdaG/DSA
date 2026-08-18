c0a9bb20c79fbc5b6cc5a6f0d6e08bf3bba91745
NarayanaGowdaG <narayanagowda658@gmail.com>
2026-08-18 16:36:31 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (75.60%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -12,8 +12,7 @@ class Solution {
                 return mid;
 
      else if (nums[mid] > nums[mid - 1]) low = mid + 1;
-     else if (nums[mid] > nums[mid + 1]) high= mid - 1;
-            else low = mid + 1;
+            else high = mid - 1;
         }
      
         return -1;
