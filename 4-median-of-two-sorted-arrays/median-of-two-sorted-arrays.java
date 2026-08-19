80a95e3711b1a22f5adff6ccda27daabb5ebf0a1
NarayanaGowdaG <narayanagowda658@gmail.com>
2026-08-19 21:30:01 +0530

    Time: 1 ms (100.00%) | Memory: 49 MB (15.85%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,22 +1,42 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
          int n1 = nums1.length, n2 = nums2.length;
-       int[] merged = new int[n1 + n2];
-        int i = 0, j = 0, k = 0;
+        int n = n1 + n2; 
+        int ind2 = n / 2;
+        int ind1 = ind2 - 1;
+        int cnt = 0;
+        int el1 = -1, el2 = -1;
+        int i = 0, j = 0;
         while (i < n1 && j < n2) {
-            if (nums1[i] < nums2[j]) merged[k++] = nums1[i++];
-            else merged[k++] = nums2[j++];
+            if (nums1[i] < nums2[j]) {
+                if (cnt == ind1) el1 = nums1[i];
+                if (cnt == ind2) el2 = nums1[i];
+                cnt++;
+                i++;
+            } else {
+                if (cnt == ind1) el1 = nums2[j];
+                if (cnt == ind2) el2 = nums2[j];
+                cnt++;
+                j++;
+            }
         }
-        while (i < n1) merged[k++] = nums1[i++];
-        while (j < n2) merged[k++] = nums2[j++];
-        int n = n1 + n2;
-        if (n % 2 == 1) 
-            return (double) merged[n / 2];
-        
-        else
-       return ((double) merged[n / 2] +  merged[(n / 2) - 1]) / 2;
+        while (i < n1) {
+            if (cnt == ind1) el1 = nums1[i];
+            if (cnt == ind2) el2 = nums1[i];
+            cnt++;
+            i++;
+        }
+        while (j < n2) {
+            if (cnt == ind1) el1 = nums2[j];
+            if (cnt == ind2) el2 = nums2[j];
+            cnt++;
+            j++;
+        }
+        if (n % 2 == 1) {
+            return (double) el2;
+        }
 
-
+        return ((double)el1 + el2) / 2;
         
     }
 }
