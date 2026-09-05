38a64bcf689201c29adf25f4ab4dfc6cb591cfb6
Narayana Gowda G <narayanagowda658@gmail.com>
2026-09-05 09:15:55 +0530

    Time: 7 ms (11.00%) | Memory: 46.6 MB (64.36%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,61 +1,54 @@
 class Solution {
-    private boolean isSafe(List<String> board, int row, int col) {
-        int r = row, c = col;
-
-        while (r >= 0 && c >= 0) {
-            if (board.get(r).charAt(c) == 'Q') return false;
-            r--;
-            c--;
-        }
-
-        r = row;
-        c = col;
-
-        while (r >= 0) {
-            if (board.get(r).charAt(c) == 'Q') return false;
-            r--;
-        }
-
-        r = row;
-        c = col;
-
-        while (r >= 0 && c < board.get(0).length()) {
-            if (board.get(r).charAt(c) == 'Q') return false;
-            r--;
-            c++;
-        }
-
-        return true;
+    public List<List<String>> solveNQueens(int n) {
+         List<List<String>> ans=new ArrayList<>();
+         List<String> board=new ArrayList<>();
+         String str="";
+         for(int i=0;i<n;i++){
+            str=str+".";
+         }
+         for(int i=0;i<n;i++){
+            board.add(str);
+         }
+         func(0,board,ans,n);
+         return ans;
+ }
+ private void func(int row, List<String> board,  List<List<String>> ans,int n){
+    if(row==n){
+        ans.add(new ArrayList<>(board));
+        return;
     }
-
-    private void func(int row, List<List<String>> ans, List<String> board) {
-        if (row == board.size()) {
-            ans.add(new ArrayList<>(board));
-            return;
-        }
-
-        for (int col = 0; col < board.get(0).length(); col++) {
-            if (isSafe(board, row, col)) {
-                char[] rowArr = board.get(row).toCharArray();
+    for(int col=0;col<n;col++){
+        if(placingIsPossible(board,row,col,n)){
+             char[] rowArr = board.get(row).toCharArray();
                 rowArr[col] = 'Q';
                 board.set(row, new String(rowArr));
-
-                func(row + 1, ans, board);
-
-                rowArr[col] = '.';
+                func(row + 1, board, ans,n);
+                  rowArr[col] = '.';
                 board.set(row, new String(rowArr));
-            }
         }
     }
-
-    public List<List<String>> solveNQueens(int n) {
-        List<List<String>> ans = new ArrayList<>();
-        List<String> board = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            board.add(".".repeat(n));
-        }
-
-        func(0, ans, board);
-        return ans;
+ }
+ private boolean placingIsPossible(List<String> board,int row,int col,int n){
+    int r,c;
+     r=row-1;
+    while(r>=0){
+        if(board.get(r).charAt(col)=='Q')return false;
+        r--;
     }
+     r=row-1;
+     c=col+1;
+     while(r>=0 && c<n){
+        if(board.get(r).charAt(c)=='Q')return false;
+        r--;
+        c++;
+    }
+    r=row-1;
+    c=col-1;
+     while(r>=0 && c>=0){
+        if(board.get(r).charAt(c)=='Q')return false;
+        r--;
+        c--;
+    }
+    return true;
+ }
 }
