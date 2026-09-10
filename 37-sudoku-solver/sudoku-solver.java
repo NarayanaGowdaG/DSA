db4e1d703abad87557b352f93aabb91e14066d24
Narayana Gowda G <narayanagowda658@gmail.com>
2026-09-10 21:41:03 +0530

    Time: 294 ms (41.88%) | Memory: 43 MB (41.53%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.java
+++ 37-sudoku-solver/sudoku-solver.java
@@ -4,42 +4,41 @@ public void solveSudoku(char[][] board) {
         solve(board);
     }
     private boolean solve(char[][] board) {
-        int n = 9;  
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] == '.') {  
-                    for (char digit = '1'; digit <= '9'; digit++) {
-                        if (areRulesMet(board, i, j, digit)) {  
-                            board[i][j] = digit;  
-                            if (solve(board)) {  
-                                return true;
-                            } else {
-                                board[i][j] = '.';  
-                            }
-                        }
-                    }
-                    return false;  
-                }
-            }
-        }
-        return true;  
+ int n = 9;  
+ for (int i = 0; i < n; i++) {
+ for (int j = 0; j < n; j++) {
+    if (board[i][j] == '.') {  
+for (char digit = '1'; digit <= '9'; digit++) {
+  if (areRulesMet(board, i, j, digit)) {  
+ board[i][j] = digit;  
+ boolean val=solve(board); 
+if(val) return true;
+board[i][j] = '.';  
+ }  
+  }
+  return false;
+ } 
+}
+}
+return true;  
     }
 
     private boolean areRulesMet(char[][] board, int row, int col, char digit) {
+        int startRow = (row/3)*3;
+        int startCol = (col/3)*3;
         for (int i = 0; i < 9; i++) {
-            if (board[row][i] == digit || board[i][col] == digit) {
+            if (board[row][i] == digit) {
                 return false;  
             }
-        }
-        int startRow = (row / 3) * 3;
-        int startCol = (col / 3) * 3;
-        for (int i = startRow; i < startRow + 3; i++) {
-            for (int j = startCol; j < startCol + 3; j++) {
-                if (board[i][j] == digit) {
+            if(board[i][col] == digit){
+                return false;
+            }
+        
+        if (board[startRow+i/3][startCol+i%3] == digit) {
                     return false;  
                 }
             }
-        }
+        
         return true;  
     }
 }
